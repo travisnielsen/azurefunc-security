7e0ac164b16b87210811173e1605e347629303e7
trniel <trn@nielski.com>
2020-01-12 22:30:03 -0600

    fix lower case header logging

--- funcapp-orders/main/index.ts
+++ funcapp-orders/main/index.ts
@@ -8,11 +8,11 @@ export default function(context: Context, req: HttpRequest): void {
 
   if (req.headers['x-ms-client-principal-name']) {
     console.info("App Service AAD Authorization is enabled. Header values (lower-case):")
-    console.info("X-MS-CLIENT-PRINCIPAL-NAME: " + req.headers['x-ms-client-principal-name']);
-    console.info("X-MS-CLIENT-PRINCIPAL-ID: " + req.headers['x-ms-client-principal-id']);
-    console.info("X-MS-TOKEN-AAD-ID-TOKEN: " + req.headers['X-MS-TOKEN-AAD-ID-TOKEN']);
-    console.info("X-MS-TOKEN-AAD-ACCESS-TOKEN: " + req.headers['X-MS-TOKEN-AAD-ACCESS-TOKEN']);
-    console.info("X-MS-TOKEN-AAD-EXPIRES-ON: " + req.headers['X-MS-TOKEN-AAD-EXPIRES-ON']);
+    console.info("x-ms-client-principal-name: " + req.headers['x-ms-client-principal-name']);
+    console.info("x-ms-client-principal-id: " + req.headers['x-ms-client-principal-id']);
+    console.info("x-ms-token-aad-id-token: " + req.headers['x-ms-token-aad-id-token']);
+    console.info("x-ms-token-aad-access-token: " + req.headers['x-ms-token-aad-access-token']);
+    console.info("x-ms-token-aad-expires-on: " + req.headers['x-ms-token-aad-expires-on']);
   }
 
   if (req.headers['X-MS-CLIENT-PRINCIPAL-NAME']) {
