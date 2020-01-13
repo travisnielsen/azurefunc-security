1981d727a54b70597467401523d886817d5ed6b1
trniel <trn@nielski.com>
2020-01-12 21:43:51 -0600

    added App Service header value logging

--- funcapp-orders/main/index.ts
+++ funcapp-orders/main/index.ts
@@ -3,5 +3,17 @@ import { AzureHttpAdapter } from '@nestjs/azure-func-http';
 import { createApp } from '../src/main.azure';
 
 export default function(context: Context, req: HttpRequest): void {
+
+  console.info("Orders API invoked")
+
+  if (process.env['X-MS-CLIENT-PRINCIPAL-NAME']) {
+    console.info("App Service AAD Authorization is enabled. Header values:")
+    console.info("X-MS-CLIENT-PRINCIPAL-NAME: " + process.env['X-MS-CLIENT-PRINCIPAL-NAME']);
+    console.info("X-MS-CLIENT-PRINCIPAL-ID: " + process.env['X-MS-CLIENT-PRINCIPAL-ID']);
+    console.info("X-MS-TOKEN-AAD-ID-TOKEN: " + process.env['X-MS-TOKEN-AAD-ID-TOKEN']);
+    console.info("X-MS-TOKEN-AAD-ACCESS-TOKEN: " + process.env['X-MS-TOKEN-AAD-ACCESS-TOKEN']);
+    console.info("X-MS-TOKEN-AAD-EXPIRES-ON: " + process.env['X-MS-TOKEN-AAD-EXPIRES-ON']);
+  }
+  
   AzureHttpAdapter.handle(createApp, context, req);
 }
